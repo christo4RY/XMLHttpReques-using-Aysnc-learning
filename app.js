24ef9e2daf479c02b326a4c9def4a457f7233cc4
christopher4RY <arkarlin486@gmail.com>
2022-08-21 21:13:42 +0630

    promise and promise chain

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
 //https://jsonplaceholder.typicode.com/todos
-let getData = (resource, callback) => {
-  let request = new XMLHttpRequest();
-  request.addEventListener("readystatechange", () => {
-    if (request.status == 200) {
-      let changeJsonFormat = JSON.parse(request.responseText);
-      callback(changeJsonFormat, undefined);
-    } else if (request.status == 404) {
-      callback(undefined, "404 not found.");
-    }
-  });
-  request.open("GET", resource);
-  request.send();
-};
-getData("https://jsonplaceholder.typicode.com/todos", (datas, err) => {
-  if (datas) {
-    for (data in datas) {
-      console.log(datas[data]);
-    }
-    getData("arkar.json", (datas, err) => {
-      if (datas) {
-        console.log(datas);
-      }
-      if (err) {
-        console.log(err);
+let getData = (resource) => {
+  return new Promise((resolve, reject) => {
+    let request = new XMLHttpRequest();
+    request.addEventListener("readystatechange", () => {
+      if (request.status == 200) {
+        let changeJsonFormat = JSON.parse(request.responseText);
+        resolve(changeJsonFormat);
+      } else if (request.status == 404) {
+        reject("404 not found.");
       }
     });
-  }
-  if (err) {
+    request.open("GET", resource);
+    request.send();
+  });
+};
+getData("https://jsonplaceholder.typicode.com/todos")
+  .then((datas) => {
+    console.log(datas);
+    return getData("arkar.json");
+  })
+  .then((datas) => {
+    console.log(datas);
+    return getData("https://jsonplaceholder.typicode.com/todos");
+  })
+  .then((datas) => {
+    console.log(datas);
+  })
+  .catch((err) => {
     console.log(err);
-  }
-});
+  });
